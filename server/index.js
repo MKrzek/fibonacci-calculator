5e9fca214e99e2e977182aa8c68935e59c1730d4
MKrzek <mkrzek@googlemail.com>
2020-04-22 15:43:01 +0100

    feat: add routes

--- server/index.js
+++ server/index.js
@@ -31,4 +31,33 @@ const redisClient = redis.createClient({
     port: keys.redisPort,
     redis_strategy:() => 1000
 });
+
 const redisPublisher = redisClient.duplicate()
+
+app.get('/', (req, res)=>{
+    res.send('hshshsshsh')
+})
+
+app.get('/values/all', async(req, res)=>{
+    const values = await pgClient.query('SELECT * from values')
+    res.send(values.rows)
+})
+app.get('values/current', async(req, res)=>{
+    redisClient.hgetall('values', (err, values)=>{
+        res.send(values)
+    })
+});
+
+app.post('/values', async(req, res)=>{
+    const index = req.body.index;
+
+     if(parseInt(index) > 40){
+         return res.status(422).send('Index too high')
+     }
+    redisClient.hset('values', index, 'Nothing yet!');
+    redisPublisher.publisher('insert', index)
+    pgClient.query('INSERT INTO VALUES(number) VALUES($1), [index]')
+
+    res.send({working: true})
+})
+
