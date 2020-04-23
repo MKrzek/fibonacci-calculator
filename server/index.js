3708550e35a9df4d2ec1e61b9d814990bdd86123
MKrzek <mkrzek@googlemail.com>
2020-04-23 16:13:03 +0100

    style: remove whitespaces

--- server/index.js
+++ server/index.js
@@ -39,9 +39,7 @@ app.get('/', (req, res)=>{
 })
 
 app.get('/values/all', async(req, res)=>{
-
     const values = await pgClient.query('SELECT * from values')
-
     res.send(values.rows)
 })
 app.get('/values/current', async(req, res)=>{
